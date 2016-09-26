92f8a7055bb6d93e4c0419b285612c44e1ee6fea
Steimann Thomas <thomas.steimann@resolvex.ch>
2016-09-26 10:23:03 +0200

    tableView done

---
+++ DoIt/Task.swift
@@ -0,0 +1,15 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by tom on 26.09.16.
+//  Copyright © 2016 tom. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
