869f4ad9534eddd6ec5d9d2e92d81360167a666a
Steimann Thomas <thomas.steimann@resolvex.ch>
2016-09-28 21:37:56 +0200

    added CreateTaskViewController

---
+++ DoIt/CreateTaskViewController.swift
@@ -0,0 +1,42 @@
+//
+//  CreateTaskViewController.swift
+//  DoIt
+//
+//  Created by tom on 28.09.16.
+//  Copyright © 2016 tom. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+    
+    var previousVC = TasksViewController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+        // Create a task from the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        
+        // Add new task to array in previous viewController
+        
+        previousVC.tasks.append(task)
+        
+    }
+    
+
+}
--- DoIt/TasksViewController.swift
+++ DoIt/TasksViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TasksViewController.swift
 //  DoIt
 //
 //  Created by tom on 26.09.16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -64,6 +64,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBAction func plusTapped(_ sender: AnyObject) {
         performSegue(withIdentifier: "addSegue", sender: nil)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! CreateTaskViewController
+        nextVC.previousVC = self
         
     }
 
